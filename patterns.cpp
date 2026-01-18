d78fac07b72f7c9789c5866a5adc318a168fdc45
Varuchi Maurya <varuchi09@gmail.com>
2026-01-18 22:04:34 +0530

    Patterns problem solved

---
+++ patterns.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     for(int i = 0; i < 4; i++){
+//         for(int j = 0; j < 4; j++){
+//             cout <<  "* "; 
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main(){
+//     for(int i = 0; i < 4; i++){
+//         for(int j = 0; j <= i; j++){
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+
+// }
+
+// int main(){
+//     for(int i = 1; i < 4; i++){
+//         for(int j = 1; j <= i; j++){
+//             cout << j << "  ";
+//         }
+//         cout << endl;
+//     }
+
+// }
