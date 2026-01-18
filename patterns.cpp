b2331c1442003bf4101d934bf0b0254607835339
Varuchi Maurya <varuchi09@gmail.com>
2026-01-18 23:14:32 +0530

    pattern problems solved

--- patterns.cpp
+++ patterns.cpp
@@ -30,11 +30,24 @@ using namespace std;
 
 // }
 
-int main(){
-    for(int i = 1; i < 5; i++){
-        for(int j = i; j <= i; j++){
-            cout << j << ' ';
-        }
-        cout << endl;
-    }
-}
+
+// int main(){
+//     for(int i = 0; i < 4; i++){
+//         for(int j = 4; j > i; j--){
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+
+// }
+
+
+// int main(){
+//     for(int i = 1; i < 4; i++){
+//         for(int j = 1; j < 4 -i+1 ; j++){
+//             cout << j << ' ';
+//         }
+//         cout << endl;
+//     }
+
+// }
