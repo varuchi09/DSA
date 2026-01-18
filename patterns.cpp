bafa29cef18796f548b370ec4e365046dc8e6fe7
Varuchi Maurya <varuchi09@gmail.com>
2026-01-18 22:27:09 +0530

    pattern problems

--- patterns.cpp
+++ patterns.cpp
@@ -29,3 +29,12 @@ using namespace std;
 //     }
 
 // }
+
+int main(){
+    for(int i = 1; i < 5; i++){
+        for(int j = i; j <= i; j++){
+            cout << j << ' ';
+        }
+        cout << endl;
+    }
+}
