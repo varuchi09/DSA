5a6aeb6114c683e0c94cc9f347414cbd665e7f26
Varuchi Maurya <varuchi09@gmail.com>
2026-01-23 02:37:26 +0530

    basic maths

---
+++ basic_maths.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int count(int num){    
+    int cnt = 0;                        // num of digits
+    while(num>0){
+        int last_d = num % 10;
+        cnt++;
+        num = int(num/10);
+    }
+    return cnt; 
+}
+
+int rev(int n){  
+    int r_num = 0;                            //reverse a num
+    while(n>0){
+        int ld = n % 10;
+        r_num = (r_num*10) + ld;
+        n = n/10;
+    }
+    return r_num;
+}
+
+int main(){
+    int digit = count(56743);
+    cout<< digit <<endl;
+    int r_num = rev(289645);
+    cout<< r_num <<endl;
+    return 0;
+}
+
