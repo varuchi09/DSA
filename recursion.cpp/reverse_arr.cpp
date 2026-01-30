5a41734ccb4948f3378dca231fc7de699e02a64e
Varuchi Maurya <varuchi09@gmail.com>
2026-01-30 21:23:50 +0530

    Recusrion

---
+++ recursion.cpp/reverse_arr.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void rev(int i,  int arr[], int n){
+    if (i >= n/2) return;
+    swap(arr[i], arr[n-i-1]);
+    rev(i+1, arr, n);
+}
+
+int main(){
+    int n;
+    cout<< " enter element "<< endl; 
+    cin>> n;
+    int arr[n];
+    cout<< " enter arr element "<< endl;
+    for(int i = 0; i<n; i++){
+        cin>> arr[i];
+    }
+    rev(0,arr,n);
+    for(int i = 0; i<n; i++){
+        cout<< arr[i] << " ";
+    }
+    return 0;
+}
---
+++ recursion.cpp/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+cout<< " enter element "<< endl; 
