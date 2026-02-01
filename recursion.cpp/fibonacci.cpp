113db1664002f103feebfea6561e5779d4fc2715
Varuchi Maurya <varuchi09@gmail.com>
2026-02-01 22:42:10 +0530

    Fibonacci using recursion

--- recursion.cpp/fibonacci.cpp
+++ recursion.cpp/fibonacci.cpp
@@ -1,6 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int fib(int n){                            // T.C = 2^n
+    if(n==0 || n==1) return n;             // S.C = n
+    return fib(n-1) + fib(n-2);
+}
+
 int main(){
+    int n;
+    cout<<"enter n "<<endl;
+    cin>> n;
+    cout<< fib(n);
     return 0;
 }
