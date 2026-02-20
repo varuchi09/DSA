292a03fca195cdbc63f1c36f164a032348770db7
Varuchi Maurya <varuchi09@gmail.com>
2026-02-20 20:57:17 +0530

    finding missing element

---
+++ arrays.cpp/missing_element.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int main() {
+//     int n = 5;                                             //brute
+//     int arr[] = {1,3,4,5};
+//     for(int i = 0; i < 5; i++){
+//         if(arr[i] != i + 1){
+//             cout<< "missing element "<< i + 1;
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
+// int main() {
+//     int n = 5;                                             //optimal (using Sum)
+//     int arr[] = {1,3,4,5};
+//     int sum = (n*(n+1))/2;
+//     int s = 0;
+//     for(int i = 0; i<n-1; i++){
+//         s+=arr[i];
+//     }
+//     cout<<"Missing element "<<sum-s;
+//     return 0;
+// }
+
+
+int main() {                                                   //using XOR
+    int n = 5;
+    int arr[] = {1,2,3,5};
+    int xor1 = 0;
+    int xor2 = 0;
+    for(int i = 0; i<n-1; i++){
+        xor2 = arr[i]^xor2;
+        xor1 = (i + 1)^xor1;
+    }
+    xor1 = xor1^n;
+    cout<< "Missing element "<< (xor2^xor1);
+    return 0;
+}
