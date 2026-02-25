b9377266febf44ed574f3de0d862562422695a27
Varuchi Maurya <varuchi09@gmail.com>
2026-02-25 14:02:27 +0530

    misisng elements

--- arrays.cpp/missing_element.cpp
+++ arrays.cpp/missing_element.cpp
@@ -13,6 +13,26 @@ using namespace std;
 //     return 0;
 // }
 
+int main()
+{ // better
+    int arr[] = {1, 2, 4, 5, 6, 7, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int hash[n + 1] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        hash[arr[i]]++;
+    }
+    for (int i = 1; i < n; i++)
+    {
+        if (hash[i] == 0)
+        {
+            cout << "missing value is " << i;
+        }
+    }
+
+    return 0;
+}
+
 // int main() {
 //     int n = 5;                                             //optimal (using Sum)
 //     int arr[] = {1,3,4,5};
@@ -25,17 +45,16 @@ using namespace std;
 //     return 0;
 // }
 
-
-int main() {                                                   //using XOR
-    int n = 5;
-    int arr[] = {1,2,3,5};
-    int xor1 = 0;
-    int xor2 = 0;
-    for(int i = 0; i<n-1; i++){
-        xor2 = arr[i]^xor2;
-        xor1 = (i + 1)^xor1;
-    }
-    xor1 = xor1^n;
-    cout<< "Missing element "<< (xor2^xor1);
-    return 0;
-}
+// int main() {                                                   //using XOR
+//     int n = 5;
+//     int arr[] = {1,2,3,5};
+//     int xor1 = 0;
+//     int xor2 = 0;
+//     for(int i = 0; i<n-1; i++){
+//         xor2 = arr[i]^xor2;
+//         xor1 = (i + 1)^xor1;
+//     }
+//     xor1 = xor1^n;
+//     cout<< "Missing element "<< (xor2^xor1);
+//     return 0;
+// }
