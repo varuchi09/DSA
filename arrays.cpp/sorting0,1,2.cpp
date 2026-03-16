a62a1064a43bdf27e8c508b195bf3a92eed02191
Varuchi Maurya <varuchi09@gmail.com>
2026-03-16 23:56:18 +0530

    soving array of 0,1,2 using Dutch National Flag Algorithm

---
+++ arrays.cpp/sorting0,1,2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[] = {0,1,2,1,1,2,1,0,0};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int low = 0, mid = 0, high = n-1;
+    while(mid <= high){
+        if(arr[mid] == 0){
+            swap(arr[low] , arr[mid]);
+            mid++;
+            low++;
+        }
+        else if(arr[mid] == 1){
+            mid++;
+        }
+        else{
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+    return 0;
+}
