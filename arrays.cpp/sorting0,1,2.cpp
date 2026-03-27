1eb6b58865b7abec8800ba339b81d55b6c1b677c
Varuchi Maurya <varuchi09@gmail.com>
2026-03-27 19:44:37 +0530

    Dutch National Flag Algo

--- arrays.cpp/sorting0,1,2.cpp
+++ arrays.cpp/sorting0,1,2.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main() {                                                // optimal dutch national flag algo
     int arr[] = {0,1,2,1,1,2,1,0,0};
     int n = sizeof(arr)/sizeof(arr[0]);
     int low = 0, mid = 0, high = n-1;
